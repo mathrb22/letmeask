ee5b1fb9e28c546e49470dfba06507d08731487f
Matheus Ribeiro <mathribe2020@gmail.com>
2021-06-27 00:30:47 -0300

    refactor: create useRoom hook

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -7,32 +7,8 @@ import { FiLogOut } from 'react-icons/fi';
 import { RoomCode } from '../components/RoomCode';
 import { FormEvent, useState } from 'react';
 import { database } from '../services/firebase';
-import { useEffect } from 'react';
 import { Question } from '../components/Question';
-
-type FirebaseQuestions = Record<
-	string,
-	{
-		author: {
-			name: string;
-			avatar: string;
-		};
-		content: string;
-		isHighlighted: boolean;
-		isAnswered: boolean;
-	}
->;
-
-type QuestionType = {
-	id: string;
-	author: {
-		name: string;
-		avatar: string;
-	};
-	content: string;
-	isHighlighted: boolean;
-	isAnswered: boolean;
-};
+import { useRoom } from '../hooks/useRoom';
 
 type RoomParams = {
 	id: string;
@@ -41,34 +17,10 @@ type RoomParams = {
 export function Room() {
 	const params = useParams<RoomParams>();
 	const [newQuestion, setNewQuestion] = useState('');
-	const [questions, setQuestions] = useState<QuestionType[]>([]);
-	const [title, setTitle] = useState('');
 	const roomId = params.id;
 	const history = useHistory();
+	const { questions, title } = useRoom(roomId);
 	const { user, signOut, signInWithGoogle } = useAuth();
-
-	useEffect(() => {
-		const roomRef = database.ref(`rooms/${roomId}`);
-
-		roomRef.on('value', (room) => {
-			const databaseRoom = room.val();
-			const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
-			const parsedQuestions = Object.entries(firebaseQuestions).map(
-				([key, value]) => {
-					return {
-						id: key,
-						content: value.content,
-						author: value.author,
-						isHighlighted: value.isHighlighted,
-						isAnswered: value.isAnswered,
-					};
-				}
-			);
-
-			setTitle(databaseRoom.title);
-			setQuestions(parsedQuestions);
-		});
-	}, [roomId]);
 
 	async function handleSignOut() {
 		await signOut();
